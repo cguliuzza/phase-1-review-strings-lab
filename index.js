ba7589b09787faf42843f4ca314921a9f84a85a2
christinaguliuzza <christina.guliuzza@gmail.com>
2021-08-09 10:57:00 -0700

    commit tree 4e39eb005d90dbc296ef8e125becc2837155b64a

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Simba the cat";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage  = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
